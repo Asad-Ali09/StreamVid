1c1e34ec229828d7a385c9af887eece44a76bdf0
Asad Shafiq <masadali5433@gmail.com>
2024-06-21 16:56:42 +0500

    setup server

---
+++ server/src/index.ts
@@ -0,0 +1,35 @@
+import "express-async-errors";
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL!,
+    credentials: true,
+  })
+);
+
+const port = process.env.PORT || 5500;
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).send("Hello World!");
+});
+
+mongoose
+  .connect(process.env.MONGO_URI!)
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+  .catch((err) => console.log(err));
